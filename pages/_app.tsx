ca01a22e6117dc6fa6d94c198f3d755bd91ffc12
Haneul Lee <hnlee@cochlear.ai>
2022-07-12 18:51:09 +0200

    tailwind css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,24 @@
+import useUser from "lib/client/useUser";
 import { SWRConfig } from "swr";
-import "../global.css";
+import "../styles/globals.css";
+
+function CustomUser() {
+    const { user } = useUser();
+    return null;
+}
 
 export default function App({ Component, pageProps }: any) {
-  return (
-    <SWRConfig
-      value={{
-        fetcher: (url: string) => fetch(url).then((response) => response.json())
-      }}
-    >
-      <Component {...pageProps} />
-    </SWRConfig>
-  );
+    return (
+        <SWRConfig
+            value={{
+                fetcher: (url: string) =>
+                    fetch(url).then((response) => response.json()),
+            }}
+        >
+            <div className="mx-auto w-full max-w-xl">
+                <CustomUser />
+                <Component {...pageProps} />
+            </div>
+        </SWRConfig>
+    );
 }
