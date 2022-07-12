92a372265a8228746a555be3b8123ac6afbd5670
Haneul Lee <hnlee@cochlear.ai>
2022-07-12 18:12:02 +0200

    add log-in/sign-up page

---
+++ pages/log-in.tsx
@@ -0,0 +1,48 @@
+import useMutation from "lib/client/useMutation";
+import useUser from "lib/client/useUser";
+import { NextPage } from "next";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+const Enter: NextPage = () => {
+    const { mutateUser } = useUser();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm();
+    const [enter, { loading, data }] = useMutation("/api/users/enter");
+
+    const onValid = (validForm) => {
+        if (loading) return;
+        enter(validForm);
+        reset();
+    };
+
+    useEffect(() => {
+        if (data && data.ok) {
+            mutateUser();
+        }
+    }, [data]);
+    return (
+        <>
+            <h2>Log in</h2>
+            <form onSubmit={handleSubmit(onValid)}>
+                <div>
+                    <label htmlFor="email">Email : </label>
+                    <input
+                        {...register("email", {
+                            required: "Please write down your email.",
+                        })}
+                        type="email"
+                    />
+                    {errors.email?.message}
+                </div>
+                <input type="submit" value="Log in"></input>
+            </form>
+        </>
+    );
+};
+
+export default Enter;
---
+++ pages/sign-up.tsx
@@ -0,0 +1,61 @@
+import useMutation from "lib/client/useMutation";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+const CreateAccount: NextPage = () => {
+    const router = useRouter();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm();
+    const [create, { loading, data }] = useMutation("/api/users/create");
+
+    const onValid = (validForm) => {
+        if (loading) return;
+        create(validForm);
+        reset();
+    };
+    useEffect(() => {
+        if (data) {
+            if (data.ok) {
+                router.push("/log-in");
+            } else if (data.error) {
+                window.confirm(data.error);
+            }
+        }
+    }, [data, router]);
+    return (
+        <>
+            <h2>Create Account</h2>
+            <form onSubmit={handleSubmit(onValid)}>
+                <div>
+                    <label htmlFor="email">Email : </label>
+                    <input
+                        {...register("email", {
+                            required: "Please write down your email.",
+                        })}
+                        type="email"
+                    />
+                    {errors.email?.message}
+                </div>
+                <div>
+                    <label htmlFor="name">Name : </label>
+                    <input
+                        {...register("name", {
+                            required: "Please write down your name.",
+                        })}
+                        type="name"
+                    />
+                    {errors.name?.message}
+                </div>
+                <input type="submit" value="Create Account"></input>
+            </form>
+        </>
+    );
+};
+
+export default CreateAccount;
